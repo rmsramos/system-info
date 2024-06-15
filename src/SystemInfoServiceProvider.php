1971c26dc67c3fc14560e1cea093d644ec1d3ceb
Rômulo Ramos <rmsramos@gmail.com>
2024-06-14 23:05:46 -0300

    refactoring SysteminfoServiceProvider

--- src/SystemInfoServiceProvider.php
+++ src/SystemInfoServiceProvider.php
@@ -2,19 +2,11 @@
 
 namespace Rmsramos\SystemInfo;
 
-use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Rmsramos\SystemInfo\Commands\SystemInfoCommand;
-use Rmsramos\SystemInfo\Testing\TestsSystemInfo;
 
 class SystemInfoServiceProvider extends PackageServiceProvider
 {
@@ -24,34 +16,7 @@ class SystemInfoServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('rmsramos/system-info');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
+        $package->name(static::$name);
 
         if (file_exists($package->basePath('/../resources/views'))) {
             $package->hasViews(static::$viewNamespace);
@@ -69,26 +34,6 @@ class SystemInfoServiceProvider extends PackageServiceProvider
             $this->getAssets(),
             $this->getAssetPackageName()
         );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/system-info/{$file->getFilename()}"),
-                ], 'system-info-stubs');
-            }
-        }
-
-        // Testing
-        Testable::mixin(new TestsSystemInfo());
     }
 
     protected function getAssetPackageName(): ?string
@@ -102,53 +47,7 @@ class SystemInfoServiceProvider extends PackageServiceProvider
     protected function getAssets(): array
     {
         return [
-            // AlpineComponent::make('system-info', __DIR__ . '/../resources/dist/components/system-info.js'),
             Css::make('system-info-styles', __DIR__ . '/../resources/dist/system-info.css'),
-            Js::make('system-info-scripts', __DIR__ . '/../resources/dist/system-info.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            SystemInfoCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_system-info_table',
         ];
     }
 }
